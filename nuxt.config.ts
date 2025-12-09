4ba8e498c23d6fffbc189847bcca0b4e48a3e15c
Abdelrhman Mohamed <ami_2080@yahoo.com>
2025-12-09 05:12:24 +0200

    use vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  modules: ["vuetify-nuxt-module"],
   app: {
     head: {
       title: "Abdelrhman Mohamed",
