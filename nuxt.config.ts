d2c690ffea37a6680a104b526023ba32b481cead
Abdelrhman Mohamed <ami_2080@yahoo.com>
2025-12-09 05:11:32 +0200

    Use tailwind/vite 4.1.17

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
-  modules: ["@nuxtjs/tailwindcss"],
+  devtools: { enabled: true },
   app: {
     head: {
       title: "Abdelrhman Mohamed",
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { name: "description", content: "Abdelrhman Mohamed frontend team lead" },
+        {
+          name: "description",
+          content: "Abdelrhman Mohamed frontend team lead",
+        },
       ],
       link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
     },
   },
-  devtools: { enabled: true },
+  css: ["./app/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
